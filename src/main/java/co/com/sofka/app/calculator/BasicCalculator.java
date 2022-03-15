ae63c5ff6c088ed17a08b4c0fc19e35836360add
mathias labora martirena <mathiaslabora@gmail.com>
2022-03-15 01:52:37 -0300

    finalizado para entrega.

--- src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -5,19 +5,22 @@ import org.slf4j.LoggerFactory;
 
 public class BasicCalculator {
     private static final Logger logger =  LoggerFactory.getLogger(BasicCalculator.class);
-    public Long sum(Long number1, Long number2) {
+    public long sum(Long number1, Long number2) {
         logger.info( "Summing {} + {}", number1, number2 );
         return number1 + number2;
     }
-    public Long res(Long number1, Long number2) {
+    public long res(Long number1, Long number2) {
         logger.info( "Subtracting {} + {}", number1, number2 );
         return number1 - number2;
     }
-    public Long div(Long number1, Long number2) {
+    public long div(Long number1, Long number2) {
         logger.info( "dividing {} + {}", number1, number2 );
+        if(number1 == 0 || number2==0){
+            return 0;
+        }
         return number1 / number2;
     }
-    public Long mul(Long number1, Long number2) {
+    public long mul(Long number1, Long number2) {
         logger.info( "multiplying {} + {}", number1, number2 );
         return number1 * number2;
     }
